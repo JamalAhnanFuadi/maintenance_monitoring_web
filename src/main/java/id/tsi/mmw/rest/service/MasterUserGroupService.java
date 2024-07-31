e2cc3743dd8551d8fcc7f7b3c4ecf0f8e141774a
@bmp01 <bachtiar.madya.p@gmail.com>
2024-07-31 19:17:44 +0700

    impleennt master user

---
+++ src/main/java/id/tsi/mmw/rest/service/MasterUserGroupService.java
@@ -0,0 +1,75 @@
+package id.tsi.mmw.rest.service;
+
+import id.tsi.mmw.model.User;
+
+import javax.annotation.security.PermitAll;
+import javax.ws.rs.*;
+import javax.ws.rs.core.Response;
+import java.util.List;
+
+public class MasterUserGroupService extends BaseService {
+
+    @inject
+    private MasterUserGroupController masterUserGroupController;
+
+    public MasterUserGroupService() {
+        log = getLogger(this.getClass());
+    }
+
+    @GET
+    @PermitAll
+    public Response getUserList() {
+        final String methodName = "getUserList";
+        start(methodName);
+
+        List<User> userList;
+        userList = masterUserGroupController.getUserList();
+        completed(methodName);
+        return buildSuccessResponse(userList);
+    }
+
+    @POST
+    public Response create(User user) {
+        final String methodName = "create";
+        Response response = null;
+        start(methodName);
+
+        boolean result = masterUserGroupController.create(user);
+        completed(methodName);
+        return response;
+    }
+
+    @PUT
+    public Response update(User user) {
+        final String methodName = "update";
+        Response response = buildSuccessResponse();
+        start(methodName);
+        user.setUid();
+        boolean result = masterUserGroupController.update(user);
+
+        completed(methodName);
+        return response;
+    }
+
+    @DELETE
+    @Path("{uid}")
+    public Response delete(@PathParam("uid") String uid) {
+        final String methodName = "delete";
+        start(methodName);
+
+        Response response = buildBadRequestResponse();
+        User user = masterUserGroupController.getUserByUid(uid);
+
+        if (user.getUid() != null) {
+            boolean result = masterUserGroupController.delete(uid);
+            if (result) {
+                response = buildSuccessResponse();
+            }
+        }
+        completed(methodName);
+        return response;
+
+
+    }
+    
+}
